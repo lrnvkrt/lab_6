99c3c91b89024d3194a14da4093e37f7128192b8
lrnvkrt <krist1na.larion0va@yandex.ru>
2023-11-02 12:18:35 +0300

    Updated Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        System.out.println("Задание 1. Это добавлено для первого коммита из четырёх.");
     }
 }
