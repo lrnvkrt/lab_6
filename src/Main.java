b67b03c4b65833f9f97c0a7a5b94efd499c30c8f
lrnvkrt <krist1na.larion0va@yandex.ru>
2023-11-02 12:39:11 +0300

    Updated Main class. Added third output.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Задание 2. Первый коммит из четырёх!");
+        System.out.println("Задание 2. Второй коммит из четырёх!");
     }
 }
 
